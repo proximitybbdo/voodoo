9c2b7c879cca7d9f2913f3791fa2b82887f72b15
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-01-24 22:26:56 +0100

    updated config with some new needles

--- config.js
+++ config.js
@@ -7,27 +7,36 @@ config.init({
   concat: {
 
     // The core library files
-    "dist/debug/js/libs.js": [
-      "assets/js/libs/jquery.js",
-      "assets/js/libs/underscore.js",
-      "assets/js/libs/backbone.js"
+    "assets/js/libs.js": [
+      "assets/js/libs/dd_belatedpng.js",
+      "assets/js/libs/jquery-1.7.1.min.js",
+      "assets/js/libs/less.js",
+      "modernizr-2.0.6.min.js",
+      "selectivizr.js"
     ],
 
     // Application files
-    "dist/debug/js/app.js": ["app/*.js", "app/modules/*.js"],
+    "assets/js/app.js": ["assets/js/*.js"],
 
     // Your CSS
-    "dist/debug/css/style.css": ["assets/css/*.css"]
+    // TODO: we won't need this I guess, since
+    // we already have a strong include system within
+    // our style.less, so generally we stick with one css ?
+    // "dist/debug/css/style.css": ["assets/css/*.css"]
   },
   
+  // TODO: how do we work on this, in combo with the concat?
+  // I am guessing: first minify to name.min.js, than add that
+  // min to the concat? Or the other way around?
   min: {
     "dist/release/js/libs.js": ["dist/debug/js/libs.js"],
     "dist/release/js/app.js": ["dist/debug/js/app.js"],
     "dist/release/js/templates.js": ["dist/debug/js/templates.js"]
   },
 
+  // testing only
   mincss: {
-    "dist/release/css/style.css": ["dist/debug/css/style.css"]
+    "assets/css/style.css": ["assets/css/style.css"]
   },
 
   watch: {
@@ -47,4 +56,4 @@ config.init({
 });
 
 // Run the following tasks...
-task.registerTask("default", "lint:files");
+task.registerTask("default", "lint:files concat");
